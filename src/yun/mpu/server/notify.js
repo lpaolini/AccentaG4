9321b0c1daf5b7b40010d154e24fb10f2371ab7c
Luca Paolini <luca.paolini@fao.org>
2017-07-17 17:59:48 +0200

    switch from ssmtp to mstmp

--- src/yun/mpu/server/notify.js
+++ src/yun/mpu/server/notify.js
@@ -4,15 +4,14 @@ module.exports = Notify;
 
 function Notify(config) {
 	if (config) {
-		console.log('email notifications from: ' + config.from + ' to: ' + config.to);
+		console.log('email notifications sent to: ' + config.to);
 		return function (message) {
-			var ssmtp = spawn('ssmtp', [config.to], {stdio: ['pipe', process.stdout, process.stderr]});
-			ssmtp.stdin.write('From: ' + config.from + '\n');
-			ssmtp.stdin.write('To: ' + config.to + '\n');
-			ssmtp.stdin.write('Subject: ' + message + '\n');
-			ssmtp.stdin.write('\n');
-			ssmtp.stdin.write(message + '\n');
-			ssmtp.stdin.end();
+			var mail = spawn('msmtp', ['--from=dummy', '--read-recipients'], {stdio: ['pipe', process.stdout, process.stderr]});
+			mail.stdin.write('To: ' + config.to + '\n');
+			mail.stdin.write('Subject: ' + message + '\n');
+			mail.stdin.write('\n');
+			mail.stdin.write(message + '\n');
+			mail.stdin.end();
 			console.log('notification: ' + message);
 		}
 	} else {
