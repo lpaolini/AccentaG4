8f516d1953f1d2762279cb52c48f8dcec0b1730b
Luca Paolini <luca.paolini@fao.org>
2019-03-30 18:00:35 +0100

    Reformatted code.

--- src/yun/mcu/AccentaG4/AccentaG4.cpp
+++ src/yun/mcu/AccentaG4/AccentaG4.cpp
@@ -5,175 +5,208 @@
 
 #include "AccentaG4.h"
 
-AccentaG4::AccentaG4(uint8_t rxPin, uint8_t txPin, 
-	uint8_t setPin, uint8_t abortPin, uint8_t intPin, uint8_t paPin, 
-	void (*sendMessage)(String msg)) : serial(rxPin, txPin) {
-	this->setPin = setPin;
-	this->abortPin = abortPin;
-	this->intPin = intPin;
-	this->paPin = paPin;
-	this->sendMessage = sendMessage;
-	pinMode(setPin, INPUT_PULLUP);
-	pinMode(abortPin, INPUT_PULLUP);
-	pinMode(intPin, INPUT_PULLUP);
-	pinMode(paPin, INPUT_PULLUP);
+AccentaG4::AccentaG4(uint8_t rxPin, uint8_t txPin, uint8_t setPin,
+                     uint8_t abortPin, uint8_t intPin, uint8_t paPin,
+                     void (*sendMessage)(String msg))
+    : serial(rxPin, txPin) {
+    this->setPin = setPin;
+    this->abortPin = abortPin;
+    this->intPin = intPin;
+    this->paPin = paPin;
+    this->sendMessage = sendMessage;
+    pinMode(setPin, INPUT_PULLUP);
+    pinMode(abortPin, INPUT_PULLUP);
+    pinMode(intPin, INPUT_PULLUP);
+    pinMode(paPin, INPUT_PULLUP);
 }
 
 void AccentaG4::begin() {
-	serial.begin(BUS_SPEED);
-	tx.last = millis();
+    serial.begin(BUS_SPEED);
+    tx.last = millis();
 }
 
-void AccentaG4::end() {
-	serial.end();
-}
+void AccentaG4::end() { serial.end(); }
 
 void AccentaG4::sendKey(char key) {
     switch (key) {
-      case '0': tx.queue.push(K_0); break;
-      case '1': tx.queue.push(K_1); break;
-      case '2': tx.queue.push(K_2); break;
-      case '3': tx.queue.push(K_3); break;
-      case '4': tx.queue.push(K_4); break;
-      case '5': tx.queue.push(K_5); break;
-      case '6': tx.queue.push(K_6); break;
-      case '7': tx.queue.push(K_7); break;
-      case '8': tx.queue.push(K_8); break;
-      case '9': tx.queue.push(K_9); break;
-      case 'c': tx.queue.push(K_CHIME); break;
-      case 'o': tx.queue.push(K_OMIT); break;
-      case 'x': tx.queue.push(K_CANCEL); break;
-      case 'p': tx.queue.push(K_PROGRAM); break;
-      case 'v': tx.queue.push(K_CONFIRM); break;
-      case 's': tx.queue.push(K_SELECT); break;
-      case '!': tx.queue.push(K_SOS); break;
-      case '?': queryStatus(); break;    
+        case '0':
+            tx.queue.push(K_0);
+            break;
+        case '1':
+            tx.queue.push(K_1);
+            break;
+        case '2':
+            tx.queue.push(K_2);
+            break;
+        case '3':
+            tx.queue.push(K_3);
+            break;
+        case '4':
+            tx.queue.push(K_4);
+            break;
+        case '5':
+            tx.queue.push(K_5);
+            break;
+        case '6':
+            tx.queue.push(K_6);
+            break;
+        case '7':
+            tx.queue.push(K_7);
+            break;
+        case '8':
+            tx.queue.push(K_8);
+            break;
+        case '9':
+            tx.queue.push(K_9);
+            break;
+        case 'c':
+            tx.queue.push(K_CHIME);
+            break;
+        case 'o':
+            tx.queue.push(K_OMIT);
+            break;
+        case 'x':
+            tx.queue.push(K_CANCEL);
+            break;
+        case 'p':
+            tx.queue.push(K_PROGRAM);
+            break;
+        case 'v':
+            tx.queue.push(K_CONFIRM);
+            break;
+        case 's':
+            tx.queue.push(K_SELECT);
+            break;
+        case '!':
+            tx.queue.push(K_SOS);
+            break;
+        case '?':
+            queryStatus();
+            break;
     }
 }
 
 void AccentaG4::readPanelSignals() {
-	int signals;
-	bitWrite(signals, 0, setPin && !digitalRead(setPin));
-	bitWrite(signals, 1, abortPin && !digitalRead(abortPin));
-	bitWrite(signals, 2, intPin && !digitalRead(intPin));
-	bitWrite(signals, 3, paPin && !digitalRead(paPin));
-	setPanelSignals(signals);
+    int signals;
+    bitWrite(signals, 0, setPin && !digitalRead(setPin));
+    bitWrite(signals, 1, abortPin && !digitalRead(abortPin));
+    bitWrite(signals, 2, intPin && !digitalRead(intPin));
+    bitWrite(signals, 3, paPin && !digitalRead(paPin));
+    setPanelSignals(signals);
 }
 
 void AccentaG4::readBusMessages() {
-	// read keypad messages
-	int data;
-	while (serial.available()) {
-		data = serial.read();
-		if (data & 0x100) { // mark parity -> start of message
-			rx.data[0] = data & 0xff; // drop parity bit
-			rx.ptr = 1;
-		} else if (rx.ptr < MSG_MAXLEN) {
-			switch (rx.data[0]) {
-				case P_COMMAND:
-					if (rx.ptr < 3) {
-						// bytes 1 and 2: data
-						rx.data[rx.ptr++] = data;
-					} else if (rx.ptr == 3) {
-						// byte 3: checksum
-						if (validateChecksum(data)) {
-							setLedStatus(rx);
-						}
-						rx.ptr++;
-					}
-					break;
-				case L_COMMAND:
-					if (rx.ptr == 1 || rx.ptr < rx.data[1] + 2) {
-						// byte 1: length
-						// byte 2 to length + 1: data
-						rx.data[rx.ptr++] = data;
-					} else if (rx.ptr == rx.data[1] + 2) {
-						// byte length + 2: checksum
-						if (validateChecksum(data)) {
-							rx.data[rx.ptr] = '\0'; // terminate string
-							setLcdStatus(rx);
-						}
-						rx.ptr++;
-					}
-					break;
-				default:
-					break; // ignore other messages
-			}
-		}
-	}
+    // read keypad messages
+    int data;
+    while (serial.available()) {
+        data = serial.read();
+        if (data & 0x100) {            // mark parity -> start of message
+            rx.data[0] = data & 0xff;  // drop parity bit
+            rx.ptr = 1;
+        } else if (rx.ptr < MSG_MAXLEN) {
+            switch (rx.data[0]) {
+                case P_COMMAND:
+                    if (rx.ptr < 3) {
+                        // bytes 1 and 2: data
+                        rx.data[rx.ptr++] = data;
+                    } else if (rx.ptr == 3) {
+                        // byte 3: checksum
+                        if (validateChecksum(data)) {
+                            setLedStatus(rx);
+                        }
+                        rx.ptr++;
+                    }
+                    break;
+                case L_COMMAND:
+                    if (rx.ptr == 1 || rx.ptr < rx.data[1] + 2) {
+                        // byte 1: length
+                        // byte 2 to length + 1: data
+                        rx.data[rx.ptr++] = data;
+                    } else if (rx.ptr == rx.data[1] + 2) {
+                        // byte length + 2: checksum
+                        if (validateChecksum(data)) {
+                            rx.data[rx.ptr] = '\0';  // terminate string
+                            setLcdStatus(rx);
+                        }
+                        rx.ptr++;
+                    }
+                    break;
+                default:
+                    break;  // ignore other messages
+            }
+        }
+    }
 }
 
 void AccentaG4::setPanelSignals(int signals) {
-	if (signals != status.signals) {
-		status.signals = signals;
-		sendPanelSignals();
-	}
+    if (signals != status.signals) {
+        status.signals = signals;
+        sendPanelSignals();
+    }
 }
 
 void AccentaG4::sendPanelSignals() {
-	// SET, ABORT, INTRUDER, PANIC
-	char value[] = PANEL_SIGNALS;
-	for (int i = 0; i < 4; i++) {
-		bitRead(status.signals, i) || (value[i] = '.');
-	}
-	sendMessage("S:" + String(value));
+    // SET, ABORT, INTRUDER, PANIC
+    char value[] = PANEL_SIGNALS;
+    for (int i = 0; i < 4; i++) {
+        bitRead(status.signals, i) || (value[i] = '.');
+    }
+    sendMessage("S:" + String(value));
 }
 
 void AccentaG4::setLedStatus(struct Rx rx) {
-	status.led = rx.data[2] << 8 | rx.data[1];
-	sendLedStatus();
+    status.led = rx.data[2] << 8 | rx.data[1];
+    sendLedStatus();
 }
 
 void AccentaG4::sendLedStatus() {
-	// ZONE 1-8, UNSET, TAMPER, SOS, POWER
-	char value[] = LED_STATUS;
-	for (int i = 0; i < 12; i++) {
-		bitRead(status.led, i) || (value[i] = '.');
-	}
-	sendMessage("P:" + String(value));
+    // ZONE 1-8, UNSET, TAMPER, SOS, POWER
+    char value[] = LED_STATUS;
+    for (int i = 0; i < 12; i++) {
+        bitRead(status.led, i) || (value[i] = '.');
+    }
+    sendMessage("P:" + String(value));
 }
 
 void AccentaG4::setLcdStatus(struct Rx rx) {
-	strncpy(status.lcd, rx.data + 2, rx.ptr - 1);
-	sendLcdStatus();
+    strncpy(status.lcd, rx.data + 2, rx.ptr - 1);
+    sendLcdStatus();
 }
 
-void AccentaG4::sendLcdStatus() {
-	sendMessage("L:" + String(status.lcd));
-}
+void AccentaG4::sendLcdStatus() { sendMessage("L:" + String(status.lcd)); }
 
 void AccentaG4::sendCommand(char key) {
-	serial.stopListening(); // half-duplex bus
-	serial.write9(K_COMMAND | 0x100); // K cmd: emulate MARK parity
-	serial.write9(key & 0xff); // key: emulate SPACE parity
-	serial.write9((K_COMMAND + key) & 0xff); // checksum: emulate SPACE parity
-	serial.listen();
+    serial.stopListening();                   // half-duplex bus
+    serial.write9(K_COMMAND | 0x100);         // K cmd: emulate MARK parity
+    serial.write9(key & 0xff);                // key: emulate SPACE parity
+    serial.write9((K_COMMAND + key) & 0xff);  // checksum: emulate SPACE parity
+    serial.listen();
 }
 
 void AccentaG4::sendCommands() {
-	// send keypad commands
-	if (tx.queue.count() && millis() - tx.last > K_DELAY_MS) { // throttle tx
-		sendCommand(tx.queue.pop());
-		tx.last = millis();
-	}
+    // send keypad commands
+    if (tx.queue.count() && millis() - tx.last > K_DELAY_MS) {  // throttle tx
+        sendCommand(tx.queue.pop());
+        tx.last = millis();
+    }
 }
 
 void AccentaG4::queryStatus() {
-	sendPanelSignals();
-	sendLedStatus();
-	sendLcdStatus();
+    sendPanelSignals();
+    sendLedStatus();
+    sendLcdStatus();
 }
 
 boolean AccentaG4::validateChecksum(char expectedChecksum) {
-	char checksum = 0;
-	for (int i = 0; i < rx.ptr; i++) {
-		checksum += rx.data[i];
-	}
-	return checksum == expectedChecksum;
+    char checksum = 0;
+    for (int i = 0; i < rx.ptr; i++) {
+        checksum += rx.data[i];
+    }
+    return checksum == expectedChecksum;
 }
 
 void AccentaG4::loop() {
-	readPanelSignals();
-	readBusMessages();
-	sendCommands();
+    readPanelSignals();
+    readBusMessages();
+    sendCommands();
 }
--- src/yun/mcu/AccentaG4/AccentaG4.ino
+++ src/yun/mcu/AccentaG4/AccentaG4.ino
@@ -1,21 +1,21 @@
 #include "AccentaG4.h"
 #include "AirSensor.h"
 
-#define COMMS_RX_PIN     10
-#define COMMS_TX_PIN     11
+#define COMMS_RX_PIN 10
+#define COMMS_TX_PIN 11
 
-#define SIG_ABORT_PIN    2
-#define SIG_SET_PIN      3
-#define SIG_INT_PIN      4
-#define SIG_PA_PIN       5
+#define SIG_ABORT_PIN 2
+#define SIG_SET_PIN 3
+#define SIG_INT_PIN 4
+#define SIG_PA_PIN 5
 
-#define READY_PIN        7
-#define STATUS_LED       13
+#define READY_PIN 7
+#define STATUS_LED 13
 
-#define LINK_SPEED       115200
-#define CONSOLE_SPEED    115200
+#define LINK_SPEED 115200
+#define CONSOLE_SPEED 115200
 
-#define HEARTBEAT_MS     2000
+#define HEARTBEAT_MS 2000
 
 #define AIR_SENSOR_DELAY 10000
 
@@ -23,93 +23,91 @@ unsigned long lastMessage;
 unsigned long timestamp;
 
 void sendMessage(String msg) {
-  handleMessage(msg);
-  timestamp = millis();
+    handleMessage(msg);
+    timestamp = millis();
 }
 
 void handleMessage(String msg) {
-  // forward panel to Yun bridge
-  Serial1.println(msg);
-  // forward panel to local serial
-  Serial.println(msg);
-  lastMessage = millis();
+    // forward panel to Yun bridge
+    Serial1.println(msg);
+    // forward panel to local serial
+    Serial.println(msg);
+    lastMessage = millis();
 }
 
 void heartbeat() {
-	if (millis() - lastMessage > HEARTBEAT_MS) {
-		char age[7];
-		sprintf(age, "%lu\0", (millis() - timestamp) / 1000);
-		handleMessage("H:" + String(age));
-	}
+    if (millis() - lastMessage > HEARTBEAT_MS) {
+        char age[7];
+        sprintf(age, "%lu\0", (millis() - timestamp) / 1000);
+        handleMessage("H:" + String(age));
+    }
 }
 
-void startBridge() {
-  Serial1.begin(LINK_SPEED);
-}
+void startBridge() { Serial1.begin(LINK_SPEED); }
 
-void stopBridge() {
-  Serial1.end(); 
-}
+void stopBridge() { Serial1.end(); }
 
 void checkBridge() {
-  // Wait for the ready pin to go low, signaling that the AR3391 is fully booted.
-  if (digitalRead(READY_PIN)) {
-    Serial.println("Bridge disabled");
-    stopBridge();
-    while (digitalRead(READY_PIN)) {
-      // The pin is still high, so give the LED a quick flash to show we're waiting.
-      digitalWrite(STATUS_LED, HIGH);
-      delay(100);
-      digitalWrite(STATUS_LED, LOW);
-      delay(100);
+    // Wait for the ready pin to go low, signaling that the AR3391 is fully
+    // booted.
+    if (digitalRead(READY_PIN)) {
+        Serial.println("Bridge disabled");
+        stopBridge();
+        while (digitalRead(READY_PIN)) {
+            // The pin is still high, so give the LED a quick flash to show
+            // we're waiting.
+            digitalWrite(STATUS_LED, HIGH);
+            delay(100);
+            digitalWrite(STATUS_LED, LOW);
+            delay(100);
+        }
+        startBridge();
+        Serial.println("Bridge enabled");
+    } else {
+        digitalWrite(STATUS_LED, LOW);
     }
-    startBridge();
-    Serial.println("Bridge enabled");
-  } else {
-    digitalWrite(STATUS_LED, LOW);
-  }
 }
 
-AccentaG4 panel(COMMS_RX_PIN, COMMS_TX_PIN, SIG_SET_PIN, SIG_ABORT_PIN, SIG_INT_PIN, SIG_PA_PIN, sendMessage);
+AccentaG4 panel(COMMS_RX_PIN, COMMS_TX_PIN, SIG_SET_PIN, SIG_ABORT_PIN,
+                SIG_INT_PIN, SIG_PA_PIN, sendMessage);
 AirSensor airSensor(AIR_SENSOR_DELAY, sendMessage);
 
 void setupPanel() {
-  pinMode(READY_PIN, INPUT_PULLUP);
-  pinMode(STATUS_LED, OUTPUT);
-  digitalWrite(STATUS_LED, HIGH);
+    pinMode(READY_PIN, INPUT_PULLUP);
+    pinMode(STATUS_LED, OUTPUT);
+    digitalWrite(STATUS_LED, HIGH);
 
-  // Give a delay for the AR3391 to reset, in case we were reset as part of a reboot command.
-  // See http://playground.arduino.cc/Hardware/Yun#rebootStability, case 3.
-  delay(2500);
-  Serial.begin(CONSOLE_SPEED);
-  startBridge();
-  panel.begin();
-  Serial.println("Ready");
+    // Give a delay for the AR3391 to reset, in case we were reset as part of a
+    // reboot command. See
+    // http://playground.arduino.cc/Hardware/Yun#rebootStability, case 3.
+    delay(2500);
+    Serial.begin(CONSOLE_SPEED);
+    startBridge();
+    panel.begin();
+    Serial.println("Ready");
 }
 
-void setupAirSensor() {
-    airSensor.begin();
-}
+void setupAirSensor() { airSensor.begin(); }
 
 void setup() {
     setupPanel();
     setupAirSensor();
 }
 
-void loop() {  
-  checkBridge();
+void loop() {
+    checkBridge();
 
-  // forward local serial to panel  
-  if (Serial.available()) {
-    panel.sendKey(Serial.read());
-  }
+    // forward local serial to panel
+    if (Serial.available()) {
+        panel.sendKey(Serial.read());
+    }
 
-  // forward Yun bridge to panel
-  if (Serial1.available()) {
-    panel.sendKey(Serial1.read());
-  }
-    
-  panel.loop();
-  airSensor.loop();
-  heartbeat();
+    // forward Yun bridge to panel
+    if (Serial1.available()) {
+        panel.sendKey(Serial1.read());
+    }
+
+    panel.loop();
+    airSensor.loop();
+    heartbeat();
 }
