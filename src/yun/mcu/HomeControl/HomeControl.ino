5351c4f7f303092cb76436b22d52cd22a95f23a1
Luca Paolini <luca.paolini@fao.org>
2019-04-04 01:38:54 +0200

    Cleanup.

--- src/yun/mcu/HomeControl/HomeControl.ino
+++ src/yun/mcu/HomeControl/HomeControl.ino
@@ -4,20 +4,14 @@
 
 #define COMMS_RX_PIN 10
 #define COMMS_TX_PIN 11
-
 #define SIG_ABORT_PIN 2
 #define SIG_SET_PIN 3
 #define SIG_INT_PIN 4
 #define SIG_PA_PIN 5
-
-// #define READY_PIN 7
 #define STATUS_LED 13
 #define LINK_SPEED 115200
-
 #define CONSOLE_SPEED 115200
-
 #define HEARTBEAT_MS 2000
-
 #define SENSORS_DELAY 5000
 
 unsigned long lastMessage;
