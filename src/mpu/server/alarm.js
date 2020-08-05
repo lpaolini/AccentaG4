91dfb63cd5820665b85ec6e70cbed003e307ce00
Luca Paolini <luca.paolini@fao.org>
2020-08-05 11:30:43 +0200

    Changed intruder/panic messages.

--- src/mpu/server/alarm.js
+++ src/mpu/server/alarm.js
@@ -30,10 +30,10 @@ status
         notify(`Alarm ${state ? 'aborted' : 'reset'}`)
     )
     .on('intruder', state =>
-        notify(`INTRUDER alarm ${state ? 'activated [!]' : 'deactivated'}`)
+        notify(state ? 'INTRUDER [!]' : 'no intruder')
     )
     .on('panic', state =>
-        notify(`PANIC alarm ${state ? 'activated [!]' : 'deactivated'}`)
+        notify(state ? 'PANIC [!]' : 'no panic')
     )
     .on('autoArm', hour =>
         log.info(`Auto-arm ${hour === -1 ? 'disabled' : `enabled at ${hour}:00`}`)
